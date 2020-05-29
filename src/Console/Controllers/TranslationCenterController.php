715ac8e84744be48e5ea038b66d48eb33e7fdb38
Vladislav Dneprov <vladislav.dneprov1995@gmail.com>
2020-05-29 14:45:44 +0300

    impelement bulk imsert for redis storage

---
+++ src/Console/Controllers/TranslationCenterController.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * ---------------------------------------------------------------------
+ * @author Vladislav Dneprov <vladislav.dneprov1995@gmail.com>
+ * @link https://www.linkedin.com/in/vladislav-dneprov/ Linkedin profile
+ * @link https://github.com/kialex Github
+ * ---------------------------------------------------------------------
+ *
+ * @version 1.0.0
+ * @package express-test
+ */
+
+namespace Kialex\TranslateCenter\Console\Controllers;
+
+use Kialex\TranslateCenter\{Client, Console\Tracker\PullProgressTracker, Storage\JsonFileStorage, Storage\RedisStorage};
+use Kialex\TranslateCenter\ParcerTranslateStorage;
+use Translate\StorageManager\Contracts\TranslationStorage;
+use Translate\StorageManager\Manager;
+use Translate\StorageManager\Storage\ElasticStorage;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\console\{Controller, ExitCode};
+use yii\helpers\Console;
+
+class TranslationCenterController extends Controller
+{
+    /**
+     * List of lang code where is localy code and value is code in Translation Center
+     */
+    const LANG_CODE = [
+        'ru' => 'ru', // It is primary language!! Put your primary language first!!
+        'de' => 'de', 'en' => 'en', 'es' => 'es', 'el' => 'el', 'he' => 'he', 'it' => 'it', 'lt' => 'lt', 'mn' => 'mn',
+        'pl' => 'pl', 'ar' => 'ar', 'sr' => 'sr', 'uk' => 'uk', 'zh' => 'zh', 'tr' => 'tr'
+    ];
+
+    public $staticSources = ['static_1', 'static_2', 'static_3'];
+    public $dynamicSources = ['dynamic_1', 'dynamic_2', 'dynamic_3'];
+
+    /**
+     * Pull all static content from Translation Center
+     *
+     * @return int
+     * @throws InvalidConfigException
+     * @throws \Translate\StorageManager\Response\Exception
+     */
+    public function actionPullStaticSources()
+    {
+        return $this->pull($this->staticSources, Yii::createObject(RedisStorage::class));
+    }
+
+    /**
+     * @param string[] $groups
+     * @param TranslationStorage $storage
+     *
+     * @return int
+     * @throws InvalidConfigException
+     * @throws \Translate\StorageManager\Response\Exception
+     */
+    protected function pull($groups, TranslationStorage $storage)
+    {
+        $manager = new Manager(Yii::createObject(Client::class), $storage, new ParcerTranslateStorage());
+
+        try {
+            foreach ($groups as $group) {
+                $manager
+                    ->setTracker(new PullProgressTracker())
+                    ->updateGroup($group, array_values(self::LANG_CODE));
+            }
+        } catch (\Exception $e) {
+            Console::endProgress('Fail: ' . $e->getMessage() . PHP_EOL);
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+
+        return ExitCode::OK;
+    }
+
+    /**
+     * Pull all dynamic content from Translation Center
+     *
+     * @return int
+     * @throws InvalidConfigException
+     * @throws \Translate\StorageManager\Response\Exception
+     */
+    public function actionPullDynamicSources()
+    {
+        return $this->pull($this->dynamicSources, Yii::createObject(ElasticStorage::class));
+    }
+}
