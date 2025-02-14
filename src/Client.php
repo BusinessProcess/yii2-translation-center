006dd68afc56512158aa70cefb8f123186322ad6
Alexander <aleksandrgur@gmail.com>
2025-02-14 22:54:40 +0100

    Adding productionUrl and developmentUrl

--- src/Client.php
+++ src/Client.php
@@ -23,8 +23,8 @@ use yii\base\{BaseObject};
  */
 class Client extends BaseObject implements Api
 {
-    const PRODUCTION_URL = 'https://api.translate.center/api/v1/';
-    const DEV_URL = 'https://dev-api.translate.center/api/v1/';
+    const DEFAULT_PRODUCTION_URL = 'https://api.translate.center/api/v1/';
+    const DEFAULT_DEVELOPMENT_URL = 'https://dev-api.translate.center/api/v1/';
 
     /**
      * @var string Translate Center login
@@ -40,6 +40,16 @@ class Client extends BaseObject implements Api
      * @var string Translate Center project ID
      */
     public $projectUuid;
+
+    /**
+     * @var string Translate Center production URL
+     */
+    public $productionUrl = null;
+
+    /**
+     * @var string Translate Center development URL
+     */
+    public $developmentUrl = null;
 
     /**
      * @var bool disable/enable production mode
@@ -71,7 +81,7 @@ class Client extends BaseObject implements Api
         $this->apiClient = new ApiClient([
             'login' => $this->login,
             'password' => $this->password,
-            'api' => $this->production ? self::PRODUCTION_URL : self::DEV_URL
+            'api' => $this->production ? ($this->productionUrl ? $this->productionUrl : self::DEFAULT_PRODUCTION_URL) : ($this->developmentUrl ? $this->developmentUrl : self::DEFAULT_DEVELOPMENT_URL)
         ], new ArrayStorage());
         $this->apiClient->setAlias('projectUuid', $this->projectUuid);
     }
